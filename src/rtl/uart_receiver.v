b3ba27856442211881afb374251b55ede904023a
m4j0rt0m <major.tom.spaceworm@gmail.com>
2021-02-18 03:09:27 +0100

    remove spyglass warnings: separate fsms' sequential and combinational logic in uart_receiver

--- src/rtl/uart_receiver.v
+++ src/rtl/uart_receiver.v
@@ -41,17 +41,18 @@ module uart_receiver
   input   wire                  parity_bit_i, // uart parity bit enable
   input   wire  [DIV_SIZE-1:0]  baud_div_i,   // baudrate freq_div value
   input   wire                  rx_i,         // rx input
-  output  reg   [DATA_UART-1:0] rx_data_o,    // rx data output
-  output  reg                   rx_valid_o    // rx valid output
+  output  wire  [DATA_UART-1:0] rx_data_o,    // rx data output
+  output  wire                  rx_valid_o    // rx valid output
 );
 
   /* regs and wires */
-  reg   [DIV_SIZE-1:0]  counter_int;        // sampling counter
-  reg   [3:0]           bitcount_int;       // sampling bit count
-  reg   [2:0]           rx_trigger_int;     // metastability rx tripple trigger latching
-  wire  rx   =          rx_trigger_int[2];  // rx bit
-  reg   [DATA_UART-1:0] rx_data_int;        // sampling rx data reg
-  reg                   rx_stop_bits_int;   // checked first stop bit flag <0=false>/<1=true>
+  reg   [DATA_UART-1:0] rx_data, rx_data_d;                   // rx data output
+  reg                   rx_valid, rx_valid_d;                 // rx valid output
+  reg   [DIV_SIZE-1:0]  counter_int, counter_int_d;           // sampling counter
+  reg   [3:0]           bitcount_int, bitcount_int_d;         // sampling bit count
+  reg   [2:0]           rx_trigger_int;                       // metastability rx tripple trigger latching
+  reg   [DATA_UART-1:0] rx_data_int, rx_data_int_d;           // sampling rx data reg
+  reg                   rx_stop_bits_int, rx_stop_bits_int_d; // checked first stop bit flag <0=false>/<1=true>
 
   /* fsm parameters */
   localparam  ResetState      = 6'b000000;  // reset state
@@ -60,7 +61,7 @@ module uart_receiver
   localparam  DataUartState   = 6'b001001;  // receive data bits
   localparam  ParityBitState  = 6'b010001;  // receive parity bit
   localparam  StopBitsState   = 6'b100001;  // receive stop bits
-  reg [5:0] state;
+  reg [5:0] state, state_d;
 
   /*
     Always description:
@@ -69,118 +70,142 @@ module uart_receiver
   */
   always @ (posedge clk_i, negedge rstn_i) begin
     if(~rstn_i) begin
-      rx_trigger_int      <=  3'b111;
+      rx_trigger_int      <= 3'b111;
     end else begin
-      rx_trigger_int[0]   <=  rx_i;
-      rx_trigger_int[2:1] <=  rx_trigger_int[1:0];
+      rx_trigger_int[0]   <= rx_i;
+      rx_trigger_int[2:1] <= rx_trigger_int[1:0];
     end
   end
+  wire rx = rx_trigger_int[2]; // rx bit
 
   /*
     Always description:
     The receiver FSM responsible to read the data packet from the external UART interface and sample it correctly 
   */
-  always @ (posedge clk_i, negedge rstn_i) begin
-    if(~rstn_i) begin
-      counter_int       <=  0;
-      bitcount_int      <=  4'b0;
-      rx_data_int       <=  {DATA_UART{1'b0}};
-      rx_stop_bits_int  <=  1'b0;
-      rx_data_o         <=  {DATA_UART{1'b0}};
-      rx_valid_o        <=  1'b0;
-      state             <=  ResetState;
-    end
-    else begin
-      case(state)
-        ResetState: begin     // "reset" fsm state
-          counter_int       <=  0;
-          bitcount_int      <=  4'b0;
-          rx_data_int       <=  {DATA_UART{1'b0}};
-          rx_stop_bits_int  <=  1'b0;
-          rx_data_o         <=  {DATA_UART{1'b0}};
-          rx_valid_o        <=  1'b0;
-          state             <=  IdleState;
+  // --- fsm: comb
+  always @ (*) begin
+    counter_int_d       = counter_int;
+    bitcount_int_d      = bitcount_int;
+    rx_data_int_d       = rx_data_int;
+    rx_stop_bits_int_d  = rx_stop_bits_int;
+    rx_data_d           = rx_data;
+    rx_valid_d          = rx_valid;
+    state_d             = state;
+    case(state)
+      ResetState: begin     // "reset" fsm state
+        counter_int_d       = {DIV_SIZE{1'b0}};
+        bitcount_int_d      = 4'b0;
+        rx_data_int_d       = {DATA_UART{1'b0}};
+        rx_stop_bits_int_d  = 1'b0;
+        rx_data_d           = {DATA_UART{1'b0}};
+        rx_valid_d          = 1'b0;
+        state_d             = IdleState;
+      end
+      IdleState: begin      // "idle" fsm state
+        if(~rx & en_i) begin  // uart enabled and incoming data (start bit)
+          counter_int_d = baud_div_i>>1;  // freq_div / 2
+          state_d       = StartBitState;  // go to receive start bit
         end
-        IdleState: begin      // "idle" fsm state
-          if(~rx & en_i) begin  // uart enabled and incoming data (start bit)
-            counter_int <=  baud_div_i>>1;  // freq_div / 2
-            state       <=  StartBitState;  // go to receive start bit
-          end
-          bitcount_int      <=  4'b0; // reset bitcount_int
-          rx_valid_o        <=  1'b0; // no rx_data_int valid
-          rx_stop_bits_int  <=  1'b0; // reset checked fist stop bit flag
+        bitcount_int_d      = 4'b0; // reset bitcount_int
+        rx_valid_d          = 1'b0; // no rx_data_int valid
+        rx_stop_bits_int_d  = 1'b0; // reset checked fist stop bit flag
+      end
+      StartBitState: begin  // "receive start bit" fsm state
+        if(counter_int >= baud_div_i-1) begin // freq_div reached //How does this part work???
+          counter_int_d = 0;
+          state_d       = DataUartState;
         end
-        StartBitState: begin  // "receive start bit" fsm state
-          if(counter_int>=baud_div_i-1) begin // freq_div reached //How does this part work???
-            counter_int <=  0;
-            state       <=  DataUartState;
-          end
-          else
-            counter_int <=  counter_int + 1;
-        end
-        DataUartState: begin  // "receive data bits" fsm state
-          if(counter_int >= (baud_div_i-1)) begin // freq_div reached
-            rx_data_int[DATA_UART-1]    <=  rx;
-            rx_data_int[DATA_UART-2:0]  <=  rx_data_int[DATA_UART-1:1];
-            bitcount_int                <=  bitcount_int + 4'b1;
-            counter_int                 <=  0;
-            if(bitcount_int==DATA_UART-1) begin // finished receiving data
-              if(parity_bit_i) begin
-                state <=  ParityBitState;
-              end else begin
-                state <=  StopBitsState;
-              end
+        else
+          counter_int_d = counter_int + 1;
+      end
+      DataUartState: begin  // "receive data bits" fsm state
+        if(counter_int >= (baud_div_i-1)) begin // freq_div reached
+          rx_data_int_d[DATA_UART-1]    = rx;
+          rx_data_int_d[DATA_UART-2:0]  = rx_data_int[DATA_UART-1:1];
+          bitcount_int_d                = bitcount_int + 4'b1;
+          counter_int_d                 = {DIV_SIZE{1'b0}};
+          if(bitcount_int == DATA_UART-1) begin // finished receiving data
+            if(parity_bit_i) begin
+              state_d = ParityBitState;
+            end else begin
+              state_d = StopBitsState;
             end
           end
-          else
-            counter_int <=  counter_int + 1;
         end
-        ParityBitState: begin // "receive parity bit" fsm state
-          if(counter_int>=baud_div_i-1) begin // freq_div reached
-            counter_int <=  0;
-            state       <=  StopBitsState;
-          end
-          else
-            counter_int <=  counter_int + 1;
+        else
+          counter_int_d = counter_int + 1;
+      end
+      ParityBitState: begin // "receive parity bit" fsm state
+        if(counter_int >= baud_div_i-1) begin // freq_div reached
+          counter_int_d = 0;
+          state_d       = StopBitsState;
         end
-        StopBitsState: begin  // "receive stop bits" fsm state
-          if(counter_int>=(baud_div_i-1)) begin // freq_div reached
-            case(stop_bits_i)
-              0: begin    // 1 stop bit required
-                rx_data_o   <=  rx_data_int;
-                rx_valid_o  <=  1'b1;
-                state       <=  IdleState;
-              end
-              1: begin    // 2 stop bits required
-                case(rx_stop_bits_int)
-                  0: begin  // latch another stop bit
-                    rx_stop_bits_int  <=  1'b1;
-                    counter_int       <=  0;
-                  end
-                  1: begin  // return to idle state
-                    rx_data_o   <=  rx_data_int;
-                    rx_valid_o  <=  1'b1;
-                    state       <=  IdleState;
-                  end
-                endcase
-              end
-            endcase
-          end
-          else
-            counter_int <=  counter_int + 1;
+        else
+          counter_int_d = counter_int + 1;
+      end
+      StopBitsState: begin  // "receive stop bits" fsm state
+        if(counter_int >= (baud_div_i-1)) begin // freq_div reached
+          case(stop_bits_i)
+            0: begin    // 1 stop bit required
+              rx_data_d   = rx_data_int;
+              rx_valid_d  = 1'b1;
+              state_d     = IdleState;
+            end
+            1: begin    // 2 stop bits required
+              case(rx_stop_bits_int)
+                0: begin  // latch another stop bit
+                  rx_stop_bits_int_d  = 1'b1;
+                  counter_int_d       = {DIV_SIZE{1'b0}};
+                end
+                1: begin  // return to idle state
+                  rx_data_d   = rx_data_int;
+                  rx_valid_d  = 1'b1;
+                  state_d     = IdleState;
+                end
+              endcase
+            end
+          endcase
         end
-        default: begin
-          counter_int       <=  0;
-          bitcount_int      <=  4'b0;
-          rx_data_int       <=  {DATA_UART{1'b0}};
-          rx_stop_bits_int  <=  1'b0;
-          rx_data_o         <=  {DATA_UART{1'b0}};
-          rx_valid_o        <=  1'b0;
-          state             <=  ResetState;
-        end
-      endcase
+        else
+          counter_int_d = counter_int + 1;
+      end
+      default: begin
+        counter_int_d       = {DIV_SIZE{1'b0}};
+        bitcount_int_d      = 4'b0;
+        rx_data_int_d       = {DATA_UART{1'b0}};
+        rx_stop_bits_int_d  = 1'b0;
+        rx_data_d           = {DATA_UART{1'b0}};
+        rx_valid_d          = 1'b0;
+        state_d             = ResetState;
+      end
+    endcase
+  end
+
+  // --- fsm: seq
+  always @ (posedge clk_i, negedge rstn_i) begin
+    if(~rstn_i) begin
+      counter_int       <= {DIV_SIZE{1'b0}};
+      bitcount_int      <= 4'b0;
+      rx_data_int       <= {DATA_UART{1'b0}};
+      rx_stop_bits_int  <= 1'b0;
+      rx_data           <= {DATA_UART{1'b0}};
+      rx_valid          <= 1'b0;
+      state             <= ResetState;
+    end
+    else begin
+      counter_int       <= counter_int_d;
+      bitcount_int      <= bitcount_int_d;
+      rx_data_int       <= rx_data_int_d;
+      rx_stop_bits_int  <= rx_stop_bits_int_d;
+      rx_data           <= rx_data_d;
+      rx_valid          <= rx_valid_d;
+      state             <= state_d;
     end
   end
+
+  /* output assignments */
+  assign rx_data_o  = rx_data;
+  assign rx_valid_o = rx_valid;
 
 endmodule
 
